6103734ab74976de97358e6913cf0458021de99e
Abigail White <as.white@aol.com>
2021-03-23 19:46:51 -0400

    demos

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import PropsExample from './components/propsexample'
+import PropsMapping from './components/PropsMapping'
 
 function App() {
-  return <div></div>;
+  const visitedPlaces = ["Vegas", "Dallas", "Chicago", "Louisville", "Clearwater", "Pigeon Forge"]
+
+  return <div>
+    <PropsExample name="Tom" business="Myspace" />
+    <PropsMapping visited={visitedPlaces}/>
+    </div>;
 }
 
 export default App;
