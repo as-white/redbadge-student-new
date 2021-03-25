4320163b884122b9a141018b02d7f6600d8a768c
Abigail White <as.white@aol.com>
2021-03-25 18:22:32 -0400

    state example

---
+++ red-badge-demo-ts/src/components/StateExample.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class StateExample extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {currentTemp: "78", location: "Indiana"}
+        // this.handleClick = this.handleClick.bind(this);
+    }
+
+    // handleClick(){
+    //     this.setState({ currentTemp: "48" });
+    // }
+
+    handleClick = () => {
+        this.setState({ currentTemp: "25" });
+    }
+
+    render(){
+        return <div>
+            The current temperature in {this.state.location} is {this.state.currentTemp}.<br />
+            <button onClick = {this.handleClick}>Click Me To Change Temp</button>
+        </div>
+    }
+}
+
+export default StateExample;
